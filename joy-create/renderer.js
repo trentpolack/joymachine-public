943a4b9f04e69872587f0163ee24b78ab2c91896
mike stedman <github@ravuya.com>
2017-05-03 11:39:04 -0600

    import babylon, create a test scene on the right pane

--- joy-create/renderer.js
+++ joy-create/renderer.js
@@ -1,3 +1,14 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+const { shell } = require('electron');
+const BABYLON = require('./vendor/babylon/babylon.custom.js')
+const canvas = document.querySelector("#viewport");
+
+window.addEventListener('load', () => {
+  var engine = new BABYLON.Engine(canvas, true);
+  var scene = new BABYLON.Scene(engine);
+  var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
+  scene.render();
+});
