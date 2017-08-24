7cd04c33ea4f0947324fe7b86baf7403e8b6e27b
trent <trent@joy-machine.com>
2017-08-23 22:12:02 -0400

    Started maybe figuring more things out and structuring appropriately?

--- joy-create/main.js
+++ joy-create/main.js
@@ -1,7 +1,7 @@
-const electron = require('electron')
-const { ipcMain } = require('electron')
+const electron = require( 'electron' )
+const { ipcMain } = require( 'electron' )
 
-const rq = require('electron-require');
+const rq = require( 'electron-require' );
 
 // Module to control application life.
 const app = electron.app
@@ -9,63 +9,68 @@ const app = electron.app
 // Module to create native browser window.
 const BrowserWindow = electron.BrowserWindow
 
-const path = require('path')
-const url = require('url')
+const path = require( 'path' )
+const url = require( 'url' )
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+// Keep a global reference of the window object, if you don't, the window will be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
 let webContents
 
-let joyapp
-
-function createWindow() {
+function createWindow( ) {
     // Create the browser window.
-    mainWindow = new BrowserWindow({ width: 800, height: 600, show: false })
+    mainWindow = new BrowserWindow( {
+        width: 800,
+        height: 600,
+        frame: false
+//        ,
+//        webPreferences: {
+//            preload: path.join( __dirname, 'src/app/startup.js' )
+//        }
+    } )
     webContents = mainWindow.webContents
 
     // and load the index.html of the app.
-    mainWindow.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
+    mainWindow.loadURL( url.format( {
+        pathname: path.join( __dirname, 'index.html' ),
         protocol: 'file:',
         slashes: true
-    }))
+    } ) )
 
     // Open the DevTools.
     // mainWindow.webContents.openDevTools()
     // Show window when page is ready
-    mainWindow.once('ready-to-show', () => {
-        mainWindow.show();
-    })
+    mainWindow.once( 'ready-to-show', ( ) => {
+        mainWindow.show( );
+    } )
 
     // Emitted when the window is closed.
-    mainWindow.on('closed', function() {
+    mainWindow.on( 'closed', function( ) {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
         mainWindow = null
-    })
+    } )
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow)
+app.on( 'ready', createWindow )
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function() {
-    app.quit()
-})
+app.on( 'window-all-closed', function( ) {
+    app.quit( )
+} )
 
-app.on('activate', function() {
+app.on( 'activate', function( ) {
     // On OS X it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
-    if (mainWindow === null) {
-        createWindow()
+    if ( mainWindow === null ) {
+        createWindow( )
     }
-})
+} )
 
-ipcMain.on('load-mesh', function(event, args) {
+ipcMain.on( 'load-mesh', function( event, args ) {
     // Received from io.js, pass it to renderer.js
     // 	FIXME: this feels like the wrong way to do this for sure
-    webContents.send('load-mesh', args);
-})
+    webContents.send( 'load-mesh', args );
+} )
